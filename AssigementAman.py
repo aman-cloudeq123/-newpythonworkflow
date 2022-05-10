775f1e24add79f61c4358af80517733f0480a387
aman-cloudeq123 <101627403+aman-cloudeq123@users.noreply.github.com>
2022-05-10 12:19:37 +0530

    Update AssigementAman.py

--- AssigementAman.py
+++ AssigementAman.py
@@ -1,20 +1,3 @@
-# PRIME NUMBER WITHIN RANGE
-
-first_number=int(input("ENTER YOUR FIRST NUMBER : "))
-last_number=int(input("ENTER YOUR LAST NUMBER : "))
-
-print("range of prime number are :")
-
-for number in range(first_number,last_number+1):
-
-    if number > 1:
-        for i in range(2, number):
-            if(number%i==0):
-                break
-        else:
-            print(number, end=" ")
-
-
 # find largest len
 
 lst= ["11","0000001","88","345","1045"]
