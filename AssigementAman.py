c654038047c9440654d382711bc92c7cc86d9920
aman-cloudeq123 <101627403+aman-cloudeq123@users.noreply.github.com>
2022-05-12 12:32:25 +0530

    Update AssigementAman.py

--- AssigementAman.py
+++ AssigementAman.py
@@ -1,30 +1,18 @@
-import sys
+import sys    
 
-inpt=int(sys.argv[1])
-Onemax=0
+inpt=int(input("ENTER THE VALUE OF NUMBER : "))
+
+a=0
+b=1
+sum=0
 count=1
-Zeromax=0
-for i in  range(len(inpt)-1):
-    if inpt[i]==inpt[i+1]:
-        count=count+1
-        if  inpt[i+1]== "0":
-
-            if(count>Zeromax):
-                Zeromax=count
-        else:
-            if(count>Onemax):
-                Onemax=count
-    else:
-        count=1
-
-if(Zeromax<Onemax):
-    for i in range(Onemax):
-        print("1" , end="")
-
-else:
-  
-    for i in range(Zeromax):
-        print("0" , end="")       
+print("Fibonacci series :" , end=" ")
+while count<=number :
+    print(sum , end=" ")
+    count=count+1
+    a=b
+    b=sum
+    sum=a+b
 
 
 
