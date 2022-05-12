7decc1157cd6f7c81263a327ce16c0fc0fd671ed
aman-cloudeq123 <101627403+aman-cloudeq123@users.noreply.github.com>
2022-05-12 13:57:26 +0530

    Update AssigementAman.py

--- AssigementAman.py
+++ AssigementAman.py
@@ -2,41 +2,30 @@ import sys
 
 number=sys.argv[1]
 
-a=0
-b=1
-sum=0
+Onemax=0
 count=1
-print("Fibonacci series :" , end=" ")
-while count<=number :
-    print(sum , end=" ")
-    count=count+1
-    a=b
-    b=sum
-    sum=a+b
-# Onemax=0
-# count=1
-# Zeromax=0
-# for i in  range(len(inpt)-1):
-#     if inpt[i]==inpt[i+1]:
-#         count=count+1
-#         if  inpt[i+1]== "0":
+Zeromax=0
+for i in  range(len(number)-1):
+    if number[i]==number[i+1]:
+        count=count+1
+        if  number[i+1]== "0":
 
-#             if(count>Zeromax):
-#                 Zeromax=count
-#         else:
-#             if(count>Onemax):
-#                 Onemax=count
-#     else:
-#         count=1
+            if(count>Zeromax):
+                Zeromax=count
+        else:
+            if(count>Onemax):
+                Onemax=count
+    else:
+        count=1
 
-# if(Zeromax<Onemax):
-#     for i in range(Onemax):
-#         print("1" , end="")
+if(Zeromax<Onemax):
+    for i in range(Onemax):
+        print("1" , end="")
 
-# else:
+else:
   
-#     for i in range(Zeromax):
-#         print("0" , end="")  
+    for i in range(Zeromax):
+        print("0" , end="")  
 
 
 
