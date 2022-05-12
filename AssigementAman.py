4b6cb1561eb59e8a05876933ce43494c317bfb96
aman-cloudeq123 <101627403+aman-cloudeq123@users.noreply.github.com>
2022-05-12 14:03:14 +0530

    Update AssigementAman.py

--- AssigementAman.py
+++ AssigementAman.py
@@ -1,31 +1,18 @@
 import sys
 
-number=sys.argv[1]
 
-Onemax=0
-count=1
-Zeromax=0
-for i in  range(len(number)-1):
-    if number[i]==number[i+1]:
-        count=count+1
-        if  number[i+1]== "0":
+first_number = int(sys.argv[1])
+last_number = int(sys.argv[2])
 
-            if(count>Zeromax):
-                Zeromax=count
+
+
+
+for number in range(first_number,last_number+1):
+
+    if number > 1:
+        for i in range(2, number):
+            if(number%i==0):
+                break
         else:
-            if(count>Onemax):
-                Onemax=count
-    else:
-        count=1
-
-if(Zeromax<Onemax):
-    for i in range(Onemax):
-        print("1" , end="")
-
-else:
-  
-    for i in range(Zeromax):
-        print("0" , end="")  
-
-
+            print("prime numbers:",number)
 
